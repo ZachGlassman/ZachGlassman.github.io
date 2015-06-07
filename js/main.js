7065b1f074062a53c0cc55fcb4e7b582f0109631
zachglassman <zach.glassman@gmail.com>
2015-06-06 22:52:02 -0400

    trythis

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
 /**
-*MAin Angular JS application*/
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
 
-var app = angular.module('ZacharyGlassman', [
-    'ngRoute'
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
 ]);
 
-//configure routes
-
-app.config(['$routeProvider', function($routeprovider){
-    $routeProvider
-    .when("/", {templateUrl: "partials/home.html", controller:"PageCtrl" })
-    .when("/contact", {templateUrl: "partials/contact.html", controller:"PageCtrl" })
-    .when("/about", {templateUrl: "partials/about.html", controller:"PageCtrl" })
-    .when("/notes", {templateUrl: "partials/notes.html", controller:"PageCtrl" });
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
 
@@ -22,7 +47,8 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   $('.carousel').carousel({
     interval: 5000
   });
-  
+
+  // Activates Tooltips for Social Links
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
   })
