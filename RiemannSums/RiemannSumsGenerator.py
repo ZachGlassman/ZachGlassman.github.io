8f39d60a6972977144c989572522f9b5b23cad4c
zachglassman <zach.glassman@gmail.com>
2015-06-21 13:44:01 -0400

    fixed second post

--- RiemannSums/RiemannSumsGenerator.py
+++ RiemannSums/RiemannSumsGenerator.py
@@ -6,7 +6,7 @@ from bokeh.plotting import figure, output_file, save, gridplot
 from bokeh.embed import components
 from bokeh.resources import CDN
 import numpy as np
-output_file("RiemannSums.html", "Riemmann Sums",mode = 'cdn')
+output_file("RiemannSums.html", "Riemmann Sums")
 
 def myfunc(x):
     return np.sin(x) + x/8
@@ -14,38 +14,41 @@ def myfunc(x):
 x = np.linspace(-20,20,100)
 y = myfunc(x)
 #plot square, remember center is at point
-val = .5
-square_xL = np.arange(0,3 * np.pi,.5)
+val = 5
+dxx = (3 * np.pi)/val
+square_xL = np.linspace(0,3 * np.pi-dxx,val)
 square_yL = myfunc(square_xL)
-square_xR = np.arange(3 * np.pi,0,-.5)
+square_xR = np.linspace(dxx,3 * np.pi,val)
 square_yR = myfunc(square_xR)
 
+
 #now generate data for last plot
-dxx = np.linspace(.05,1,20)
-data_R = np.zeros(len(dxx))
-data_L = np.zeros(len(dxx))
-data_real = np.zeros(len(dxx))
-for ind,i in enumerate(dxx):
-    xL = np.arange(0,3 * np.pi,i)
-    xR = np.arange(3 * np.pi,0,-i)
+nn = [i for i in range(1,100,5)]
+data_R = np.zeros(len(nn))
+data_L = np.zeros(len(nn))
+data_real = np.zeros(len(nn))
+
+for ind,i in enumerate(nn):
+    dxx = (3 * np.pi)/i
+    xL = np.linspace(0,3 * np.pi-dxx,i)
+    xR = np.linspace(dxx,3 * np.pi,i)
     yL = myfunc(xL)
     yR = myfunc(xR)
-    sumL = sum([i * j for j in yL])
-    sumR = sum([i * j for j in yR])
+    sumL = np.sum([dxx * j for j in yL])
+    sumR = np.sum([dxx * j for j in yR])
     data_R[ind] = sumR
     data_L[ind] = sumL
     data_real[ind] = 2 + 9 * np.pi**2/16
 
-
-
+dxx = (3 * np.pi)/val
 source = ColumnDataSource(data=dict(leftL = square_xL,
                                     topL =square_yL,
-                                    rightL = square_xL + val,
-                                    leftR = square_xR - val,
+                                    rightL = square_xL + dxx,
+                                    leftR = square_xR - dxx,
                                     topR =square_yR,
                                     rightR = square_xR,
                                     bottom = np.zeros(len(square_xL)),
-                                    dx = val
+                                    n = val
                                     ))
 
 #setupt plots
@@ -57,7 +60,7 @@ plotL = figure(y_range=(-1.5, 2.5),title = "Left Sums",
 plotR = figure(y_range=(-1.5, 2.5),title = "Right Sums",
                x_range=(-.1,10), plot_width=350, plot_height=200)
 plotSum = figure(y_range=(6.5, 8.2),title = "Compare Sums",
-               x_range=(0,1), plot_width=350, plot_height=200)
+               x_range=(0,101), plot_width=350, plot_height=200)
                 
                 
 plot.toolbar_location = None
@@ -70,11 +73,11 @@ plot.line(x, y, line_width=3, line_alpha=0.5)
 plotL.line(x, y, line_width=3, line_alpha=0.5)
 plotR.line(x, y, line_width=3, line_alpha=0.5)
 #now last plot
-plotSum.cross(x = dxx, y = data_L, color = 'red',legend="Left")
-plotSum.circle(x = dxx, y = data_R, color = 'blue',legend="Right")
-plotSum.line(x = dxx, y = data_real, color = 'green',legend="Exact")
+plotSum.cross(x = nn, y = data_L, color = 'red',legend="Left")
+plotSum.circle(x = nn, y = data_R, color = 'blue',legend="Right")
+plotSum.line(x = nn, y = data_real, color = 'green',legend="Exact")
                    
-plotSum.xaxis.axis_label = 'dx'
+plotSum.xaxis.axis_label = 'n'
 plotSum.yaxis.axis_label = 'Sum'
 
 #source = source
@@ -122,8 +125,8 @@ callback = Callback(args=dict(source=source), code="""
     return Math.sin(x) + x/8
     };
     var data = source.get('data');
-    var dx = cb_obj.get('value');
-    data['dx'] = dx;
+    var n = cb_obj.get('value');
+    data['n'] = n;
     data['topL'] = [];
     data['bottom'] = [];
     data['leftL'] = [];
@@ -133,7 +136,8 @@ callback = Callback(args=dict(source=source), code="""
     data['rightR'] = [];
     var x_val = 0;
     var end = 3 * Math.PI;
-    var num_pts = end/dx;
+    var num_pts = n;
+    dx = (end)/n
     for (i = 0; i < num_pts; i++) {
         data['leftL'].push(x_val);
         data['topL'].push(myfunc(x_val));
@@ -153,8 +157,8 @@ callback = Callback(args=dict(source=source), code="""
     source.trigger('change');
 """    )
 
-slider = Slider(start=.01, end=1, value=val, step=.05,
-                title="dx", callback=callback)
+slider = Slider(start=1, end=80, value=val, step=2,
+                title="n", callback=callback)
  
 grid = gridplot([[plotL,plotR],[plot,plotSum]])
 grid.toolbar_location = None
