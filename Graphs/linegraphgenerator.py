9b0a3b406847111d6e347d94f6611680c76f6a5f
zachglassman <zach.glassman@gmail.com>
2015-06-20 23:31:33 -0400

    new first post

--- Graphs/linegraphgenerator.py
+++ Graphs/linegraphgenerator.py
@@ -12,11 +12,13 @@ output_file("LineGraph.html", "Line Graph")
 slope = 1
 x = np.arange(-100,101,1)
 y = slope * x
-py = [[0,0,-5],[0,0,5]]
-px = [[0,-5,-5],[0,5,5]]
+a = -5
+b = 5
+py = [[0,0,a],[0,0,b]]
+px = [[0,a,a],[0,b,b]]
 color = ['red','blue']
 #first is left, then right
-source = ColumnDataSource(data=dict(x=x,y=y,px=px,py=py,col=color))
+source = ColumnDataSource(data=dict(x=x,y=y,px=px,py=py,col=color,a=a,b=b))
 #setupt plots
 plot = figure(y_range=(-8, 8),title = "Line", x_range=(-8,8), plot_width=750, plot_height=400)
 
@@ -37,12 +39,14 @@ callback = Callback(args=dict(source=source), code="""
     var slope = cb_obj.get('value');
     x = data['x'];
     y = data['y'];
+    a = data['a'];
+    b = data['b'];
     col = data['col'];
     for(i = 0; i < x.length; i++){
         y[i] = slope * x[i];
     };
-    var ly = slope * -5;
-    var ry = slope * 5;
+    var ly = slope * a;
+    var ry = slope * b;
     if (ly > 0.0){
         col[0] = 'blue';
         col[1] = 'red';
@@ -56,13 +60,20 @@ callback = Callback(args=dict(source=source), code="""
     source.trigger('change');
 """    )
 
+slidera = Slider(start=-5, end=5, value=1, step=.1,
+                title="a", callback=callback)
+sliderb = Slider(start=-5, end=5, value=1, step=.1,
+                title="b", callback=callback)
+
 slider = Slider(start=-5, end=5, value=1, step=.1,
                 title="Slope", callback=callback)
-layout = vform(slider,plot)
+
+layout = vform(slidera,sliderb,slider,plot)
 save(layout)
 
 script, div = components(layout, CDN)
-with open('lineplotfilled.js','w') as f:
+
+with open('lineplotab.js','w') as f:
     f.write(script)
 
 #now fix for what we want
